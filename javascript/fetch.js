ed31a0a71e7c348efeda6ee3ae2012a1716996d3
Caterina Paun <caterina.paun@gmail.com>
2020-05-09 13:09:06 -0700

    JavaScript promises, async/await and fetch examples

---
+++ javascript/fetch.js
@@ -0,0 +1,16 @@
+const url = 'https://swapi.dev/api/people/5/';
+
+let fetchData = (url) => {
+  fetch(url)
+    .then((response) => response.json())
+    .then((data) => {
+      console.log(data); // object
+      console.log(data.name);
+      console.log(data.height);
+      console.log(data.mass);
+      console.log(data.birth_year);
+    })
+    .catch((error) => console.log(error));
+};
+
+fetchData(url);
