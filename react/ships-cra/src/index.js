67b50c9c1815695dd4efb911e6d2119ae6fa51b4
Caterina Paun <caterina.paun@gmail.com>
2020-05-28 07:17:42 -0700

    Add react-axe to project

--- react/ships-cra/src/index.js
+++ react/ships-cra/src/index.js
@@ -3,6 +3,11 @@ import ReactDOM from 'react-dom';
 import App from './App';
 import StarshipFleet from './StarshipFleet';
 
+if (process.env.NODE_ENV !== 'production') {
+  const axe = require('react-axe');
+  axe(React, ReactDOM, 1000);
+}
+
 const root = document.getElementById('root');
 
 ReactDOM.render(<App />, root);
