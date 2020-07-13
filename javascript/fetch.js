8d8e861ea5ed5f80b165484c278ae715a89d6347
Caterina Paun <caterina.paun@gmail.com>
2020-07-13 11:29:14 -0700

    Update: fetch example

--- javascript/fetch.js
+++ javascript/fetch.js
@@ -2,11 +2,16 @@ const url = 'https://swapi.dev/api/people/5/';
 
 let fetchData = (url) => {
   fetch(url)
-    .then((response) => response.json())
+    .then((response) => {
+      console.log(data); // body response
+      return response.json();
+    })
     .then((data) => {
       console.log(data); // object
     })
-    .catch((error) => console.log(error));
+    .catch((error) => {
+      console.log(error);
+    });
 };
 
 // fetchData(url);
